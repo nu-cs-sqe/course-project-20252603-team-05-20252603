147e6a1cef7fa79c954274460c7eceab1f684969
Helena Yuan <helenayuan2027@u.northwestern.edu>
2026-05-26 14:54:20 -0500

    G41 passes, game over false when 5 active players

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -659,4 +659,20 @@ public class GameTest {
 
         assertFalse(game.isGameOver());
     }
+
+    // G41
+    @Test
+    public void isGameOverReturnsFalseWhenGameHasFiveActivePlayers() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Player player3 = new Player("Player 3");
+        Player player4 = new Player("Player 4");
+        Player player5 = new Player("Player 5");
+        Deck deck = new Deck(new Random());
+        Game game = new Game(List.of(player1, player2, player3, player4, player5), deck);
+
+        game.setupGame();
+
+        assertFalse(game.isGameOver());
+    }
 }
