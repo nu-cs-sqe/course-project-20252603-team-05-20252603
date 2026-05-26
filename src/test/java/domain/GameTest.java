e0d3c68a07ebe2cce6cebeb6a369be23ee7dfe7c
Helena Yuan <helenayuan2027@u.northwestern.edu>
2026-05-26 12:49:57 -0500

    G27 passes, end turn changes player when 4 players

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -377,4 +377,27 @@ public class GameTest {
 
         assertEquals(player2, game.getCurrentPlayer());
     }
+
+    // G27
+    @Test
+    public void endTurnAdvancesToNextPlayerWhenCurrentPlayerIsNotLast() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Player player3 = new Player("Player 3");
+        Player player4 = new Player("Player 4");
+        Deck deck = new Deck(new Random());
+        Game game = new Game(List.of(player1, player2, player3, player4), deck);
+
+        game.setupGame();
+
+        assertEquals(player1, game.getCurrentPlayer());
+
+        game.endTurn();
+
+        assertEquals(player2, game.getCurrentPlayer());
+
+        game.endTurn();
+
+        assertEquals(player3, game.getCurrentPlayer());
+    }
 }
