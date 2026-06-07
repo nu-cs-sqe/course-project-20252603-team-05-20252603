e486ca13ad46ab6b132a733eb546cfbbb15ee21a
Helena Yuan <helenayuan2027@u.northwestern.edu>
2026-06-07 12:40:41 -0700

    Pass G88: Prefer Defuse over Shield

--- src/test/java/domain/GameCardEffectsTest.java
+++ src/test/java/domain/GameCardEffectsTest.java
@@ -420,4 +420,23 @@ public class GameCardEffectsTest {
 
         assertEquals(player2, game.getCurrentPlayer());
     }
+
+    // G88
+    @Test
+    public void defuseTakesPriorityWhenPlayerAlsoHasShield() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Game game = createStartedGame(player1, player2);
+
+        removeAll(player1, CardType.DEFUSE);
+        player1.addCard(new Card(CardType.DEFUSE));
+        player1.addCard(new Card(CardType.SHIELD));
+        emptyDeck(game.getDeck());
+        game.getDeck().insertBottom(new Card(CardType.EXPLODING_KITTEN));
+
+        game.drawCard();
+
+        assertEquals(0, player1.countCardsOfType(CardType.DEFUSE));
+        assertEquals(1, player1.countCardsOfType(CardType.SHIELD));
+    }
 }
