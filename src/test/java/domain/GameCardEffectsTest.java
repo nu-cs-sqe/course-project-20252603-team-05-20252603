f6618c5f3bacc038e9f2def03c8c2ecde0ad67e8
Helena Yuan <helenayuan2027@u.northwestern.edu>
2026-06-07 12:39:32 -0700

    Pass G84: Consume one Shield

--- src/test/java/domain/GameCardEffectsTest.java
+++ src/test/java/domain/GameCardEffectsTest.java
@@ -349,4 +349,22 @@ public class GameCardEffectsTest {
 
         assertFalse(player1.isActive());
     }
+
+    // G84
+    @Test
+    public void oneShieldIsRemovedAndPreventsElimination() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Game game = createStartedGame(player1, player2);
+
+        removeAll(player1, CardType.DEFUSE);
+        player1.addCard(new Card(CardType.SHIELD));
+        emptyDeck(game.getDeck());
+        game.getDeck().insertBottom(new Card(CardType.EXPLODING_KITTEN));
+
+        game.drawCard();
+
+        assertEquals(0, player1.countCardsOfType(CardType.SHIELD));
+        assertTrue(player1.isActive());
+    }
 }
