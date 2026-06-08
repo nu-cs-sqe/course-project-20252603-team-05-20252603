5a08d2c73317b47c4628ec8ab805ef86edc8ee06
Helena Yuan <helenayuan2027@u.northwestern.edu>
2026-06-08 12:18:42 -0700

    Pass G160: Zero Shields allows elimination

--- src/main/java/domain/CardType.java
+++ src/main/java/domain/CardType.java
@@ -5,6 +5,7 @@ public enum CardType {
     DEFUSE,
     ATTACK,
     SKIP,
+    SHIELD,
     FAVOR,
     TRADE,
     MARK,
--- src/test/java/domain/GamePlayCardTest.java
+++ src/test/java/domain/GamePlayCardTest.java
@@ -1680,4 +1680,22 @@ public class GamePlayCardTest {
         assertTrue(player1.isActive());
         assertTrue(player2.isActive());
     }
+
+    // G160
+    @Test
+    public void zeroShieldsCannotPreventEliminationWithoutDefuse() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Player player3 = new Player("Player 3");
+        Game game = createStartedGame(player1, player2, player3);
+
+        removeAll(player1, CardType.DEFUSE);
+        removeAll(player1, CardType.SHIELD);
+        emptyDeck(game.getDeck());
+        game.getDeck().insertBottom(new Card(CardType.EXPLODING_KITTEN));
+
+        game.drawCard();
+
+        assertFalse(player1.isActive());
+    }
 }
