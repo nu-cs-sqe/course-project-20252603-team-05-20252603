df3858a8fbd3e6211ec38968b9bfad4a0db6ee93
Helena Yuan <helenayuan2027@u.northwestern.edu>
2026-06-07 22:22:41 -0700

    Pass G85: Zero Defuses allows elimination

--- src/test/java/domain/GamePlayCardTest.java
+++ src/test/java/domain/GamePlayCardTest.java
@@ -580,4 +580,21 @@ public class GamePlayCardTest {
 
         assertEquals(0, player1.countCardsOfType(CardType.EXPLODING_KITTEN));
     }
+
+    // G85
+    @Test
+    public void zeroDefusesCannotPreventExplodingKittenElimination() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Player player3 = new Player("Player 3");
+        Game game = createStartedGame(player1, player2, player3);
+
+        removeAll(player1, CardType.DEFUSE);
+        emptyDeck(game.getDeck());
+        game.getDeck().insertBottom(new Card(CardType.EXPLODING_KITTEN));
+
+        game.drawCard();
+
+        assertFalse(player1.isActive());
+    }
 }
