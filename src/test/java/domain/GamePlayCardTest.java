d1f5ca46196735cd2401b99c763e265e364cb618
CyrusNadi <cyrusnadi@gmail.com>
2026-06-07 23:45:55 -0500

    Keep attacked player after first draw

--- src/test/java/domain/GamePlayCardTest.java
+++ src/test/java/domain/GamePlayCardTest.java
@@ -339,4 +339,22 @@ public class GamePlayCardTest {
 
         assertEquals(player2, game.getCurrentPlayer());
     }
+
+    // G74
+    @Test
+    public void attackedPlayerRemainsCurrentPlayerAfterFirstDraw() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Deck deck = new Deck(new Random());
+        Game game = new Game(List.of(player1, player2), deck);
+
+        game.setupGame();
+        player1.addCard(new Card(CardType.ATTACK));
+        game.playCard(CardType.ATTACK);
+        deck.insertBottom(new Card(CardType.SKIP));
+
+        game.drawCard();
+
+        assertEquals(player2, game.getCurrentPlayer());
+    }
 }
