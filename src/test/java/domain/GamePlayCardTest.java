5e1320dcde6e651773119763b4204b7908cc06cd
Helena Yuan <helenayuan2027@u.northwestern.edu>
2026-06-08 12:20:12 -0700

    Pass G163: Discard used Shield

--- src/test/java/domain/GamePlayCardTest.java
+++ src/test/java/domain/GamePlayCardTest.java
@@ -1734,4 +1734,22 @@ public class GamePlayCardTest {
 
         assertEquals(1, player1.countCardsOfType(CardType.SHIELD));
     }
+
+    // G163
+    @Test
+    public void usedShieldIsAddedToDiscardPile() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Game game = createStartedGame(player1, player2);
+
+        removeAll(player1, CardType.DEFUSE);
+        Card shield = new Card(CardType.SHIELD);
+        player1.addCard(shield);
+        emptyDeck(game.getDeck());
+        game.getDeck().insertBottom(new Card(CardType.EXPLODING_KITTEN));
+
+        game.drawCard();
+
+        assertTrue(game.getDiscardPile().contains(shield));
+    }
 }
