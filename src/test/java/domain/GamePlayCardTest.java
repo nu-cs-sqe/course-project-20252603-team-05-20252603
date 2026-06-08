d3f9ebd6090297d2232de37f8051270a1b6cff21
Helena Yuan <helenayuan2027@u.northwestern.edu>
2026-06-07 22:29:48 -0700

    Pass G94: Skip advances to next player

--- src/test/java/domain/GamePlayCardTest.java
+++ src/test/java/domain/GamePlayCardTest.java
@@ -727,4 +727,18 @@ public class GamePlayCardTest {
         assertEquals(deckSize, game.getDeck().size());
         assertEquals(player2, game.getCurrentPlayer());
     }
+
+    // G94
+    @Test
+    public void playingSkipAdvancesToNextActivePlayer() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Game game = createStartedGame(player1, player2);
+
+        player1.addCard(new Card(CardType.SKIP));
+
+        game.playCard(CardType.SKIP);
+
+        assertEquals(player2, game.getCurrentPlayer());
+    }
 }
